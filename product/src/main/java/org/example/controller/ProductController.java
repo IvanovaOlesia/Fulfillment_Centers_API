121e7f7391eeee6e43d91995d7a9e218741f55b6
Ivanova <Ivanova_olesia1993@mail.ru>
2024-12-11 17:09:53 +0700

    test controller

--- product/src/main/java/org/example/controller/ProductController.java
+++ product/src/main/java/org/example/controller/ProductController.java
@@ -76,11 +76,5 @@ public class ProductController {
         return HttpStatus.OK;
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//        Map<String, String> errors = new HashMap<>();
-//        ex.getBindingResult().getFieldErrors().forEach(error ->
-//                errors.put(error.getField(), error.getDefaultMessage()));
-//        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-//    }
+
 }
--- product/src/test/java/org/example/controller/ProductControllerTest.java
+++ product/src/test/java/org/example/controller/ProductControllerTest.java
@@ -1,73 +1,69 @@
 package org.example.controller;
 
-import org.example.dto.ProductDTO;
-import org.example.entity.Product;
-import org.example.repository.ProductRepository;
-import org.example.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.example.dto.ProductDTO;
+import org.example.entity.Product;
+import org.example.service.ProductService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import static org.mockito.Mockito.when;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @SpringBootTest
-
+@AutoConfigureMockMvc
 class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
     @Mock
-    private ProductRepository productRepository;
+    private ProductService productService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @InjectMocks
-    private ProductService productService;
+    private ProductController productController;
+
+    private final ProductDTO productDTO = ProductDTO.builder()
+            .productId("p45")
+            .status("Sellable")
+            .fulfillmentCenter("fc5")
+            .quantity(6)
+            .value(349)
+            .build();
+    private final Product product = Product.builder()
+            .productId(productDTO.getProductId())
+            .status(productDTO.getStatus())
+            .fulfillmentCenter(productDTO.getFulfillmentCenter())
+            .quantity(productDTO.getQuantity())
+            .value(productDTO.getValue())
+            .build();
 
 
     @Test
-    void create() {
-        ProductDTO productDTO = ProductDTO.builder()
-                .productId("p45")
-                .status("Sellable")
-                .fulfillmentCenter("fc5")
-                .quantity(6)
-                .value(349)
-                .build();
-        Product product = Product.builder()
-                .productId(productDTO.getProductId())
-                .status(productDTO.getStatus())
-                .fulfillmentCenter(productDTO.getFulfillmentCenter())
-                .quantity(productDTO.getQuantity())
-                .value(productDTO.getValue())
-                .build();
-        when(productRepository.save(productDTO)).thenReturn(product)
+    void createProduct() throws Exception {
+        when(productService.create(productDTO)).thenReturn(product);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productDTO)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(product.getProductId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(product.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.fulfillmentCenter").value(product.getFulfillmentCenter()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(product.getQuantity()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value(product.getValue()))
+                .andDo(print());
     }
 
-    @Test
-    void readAll() {
-    }
-
-    @Test
-    void findAllByStatus() {
-    }
-
-    @Test
-    void sumValue() {
-    }
-
-    @Test
-    void update() {
-    }
-
-    @Test
-    void delete() {
-    }
-
-    @Test
-    void loadCsv() {
-    }
 }
