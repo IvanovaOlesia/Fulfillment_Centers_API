90295b783dec656ec3837f5910e359be49c26e00
pallaaxe <ovanovala@yandex.ru>
2024-12-16 19:27:29 +0700

    test

--- product/src/test/java/org/example/controller/ProductControllerTest.java
+++ product/src/test/java/org/example/controller/ProductControllerTest.java
@@ -17,6 +17,10 @@ import org.example.entity.Product;
 import org.example.service.ProductService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -41,29 +45,65 @@ class ProductControllerTest {
             .quantity(6)
             .value(349)
             .build();
-    private final Product product = Product.builder()
-            .productId(productDTO.getProductId())
-            .status(productDTO.getStatus())
-            .fulfillmentCenter(productDTO.getFulfillmentCenter())
-            .quantity(productDTO.getQuantity())
-            .value(productDTO.getValue())
+    private final Product product1 = Product.builder()
+            .productId("p45")
+            .status("Sellable")
+            .fulfillmentCenter("fc5")
+            .quantity(6)
+            .value(349)
+            .build();
+    private final Product product2 = Product.builder()
+            .productId("p46")
+            .status("Sellable")
+            .fulfillmentCenter("fc4")
+            .quantity(8)
+            .value(525)
             .build();
 
 
     @Test
-    void createProduct() throws Exception {
-        when(productService.create(productDTO)).thenReturn(product);
+    void createProductTest() throws Exception {
+        when(productService.create(productDTO)).thenReturn(product1);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/products")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(productDTO)))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(product.getProductId()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(product.getStatus()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.fulfillmentCenter").value(product.getFulfillmentCenter()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(product.getQuantity()))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value(product.getValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.productId").value(product1.getProductId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(product1.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.fulfillmentCenter").value(product1.getFulfillmentCenter()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.quantity").value(product1.getQuantity()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.value").value(product1.getValue()))
                 .andDo(print());
     }
+    @Test
+    void readAllTest() throws Exception {
+        List<Product> products = Arrays.asList(product1,product2);
+        when(productService.readAll()).thenReturn(products);
 
+        mockMvc.perform(MockMvcRequestBuilders.get("/products")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].productId").value(product1.getProductId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].status").value(product1.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].fulfillmentCenter").value(product1.getFulfillmentCenter()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].quantity").value(product1.getQuantity()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].value").value(product1.getValue()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].productId").value(product1.getProductId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].status").value(product1.getStatus()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].fulfillmentCenter").value(product1.getFulfillmentCenter()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].quantity").value(product1.getQuantity()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].value").value(product1.getValue()))
+                .andDo(print());
+    }
+    @Test
+     void findAllByStatusTest() throws Exception {
+        List<Product> products = Arrays.asList(product1,product2);
+        when(productService.readFilter("Sellable")).thenReturn(products);
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/{status}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString("Sellable")))
+                .andExpect()
+
+    }
 }
